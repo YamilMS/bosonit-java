2db3fbedb0f133e51a8bac437fcc2aed8f83bb16
Yamil Osiris Melián Santos <yamil.melian@bosonit.com>
2023-05-18 11:54:28 +0100

    loggin done

--- demo/src/main/java/com/bosonit/demo/Block5CommandLineRunnerApplication.java
+++ demo/src/main/java/com/bosonit/demo/Block5CommandLineRunnerApplication.java
@@ -1,18 +1,25 @@
 package com.bosonit.demo;
 
-import org.springframework.boot.CommandLineRunner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
-
 public class Block5CommandLineRunnerApplication{
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Block5CommandLineRunnerApplication.class);
 
 	public static void main(String[] args) {
 		System.setProperty("spring.config.name", "application"); // Configura el nombre del archivo de configuración
-		SpringApplication.run(Block5CommandLineRunnerApplication.class, args
-		);
+		SpringApplication.run(Block5CommandLineRunnerApplication.class, args);
+		LOGGER.error("Esto es un mensaje de error"); // Log de nivel "ERROR"
+		LOGGER.warn("Esto es un mensaje de advertencia"); // Log de nivel "WARNING"
+		LOGGER.info("Esto es un mensaje informativo"); // Log de nivel "INFO"
+		LOGGER.debug("DEBUG");
+		LOGGER.trace("TRACE");
+
 	}
 
 }
---
+++ demo/src/main/java/com/bosonit/demo/Controler.java
@@ -0,0 +1,20 @@
+package com.bosonit.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class Controler {
+    @RequestMapping("/")
+    public String index() {
+        log.trace("Mensaje a nivel de TRACE");
+        log.debug("Mensaje a nivel de DEBUG");
+        log.info("Mensaje a nivel de INFO");
+        log.warn("Mensaje a nivel de WARNING");
+        log.error("Mensaje a nivel de ERROR");
+
+        return "Se han generado resultados, echa un vistazo";
+    }
+}
